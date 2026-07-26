9c2a8a7722a215e5388ec41acb54f215e2526d70
Harshada Patil <harshadapatil122436@gmail.com>
2026-07-26 18:52:21 +0530

    Fix: load model artifacts and correct predict_home_price argument order

--- Sever/server.py
+++ Sever/server.py
@@ -1,15 +1,16 @@
 from flask import Flask, request, jsonify
 import util
+
 app = Flask(__name__)
 
 @app.route('/get_location_names')
-
 def get_location_names():
     response = jsonify({
         'locations': util.get_location_names()
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
+
 @app.route('/predict_home_price', methods=['POST'])
 def predict_home_price():
     total_sqft = float(request.form['total_sqft'])
@@ -17,13 +18,13 @@ def predict_home_price():
     bhk = int(request.form['bhk_price'])
     bath = int(request.form['bath_price'])
 
-
     response = jsonify({
-        'estimated_price': util.get_estimated_price(total_sqft, location_name, bhk, bath)
+        'estimated_price': util.get_estimated_price(location_name, total_sqft, bhk, bath)
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
 if __name__ == '__main__':
-   print('Starting Python Flask Server for House Price Prediction')
-   app.run()
+    print('Starting Python Flask Server for House Price Prediction')
+    util.load_saved_artifacts()
+    app.run()
