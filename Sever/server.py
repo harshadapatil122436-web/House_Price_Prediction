20ca21b533b18c3561cd722c2e24abc4563a4609
Harshada Vijay Patil <harshadapatil122436@gmail.com>
2026-07-27 09:43:13 +0530

    Update server.py

--- Sever/server.py
+++ Sever/server.py
@@ -27,6 +27,4 @@ def predict_home_price():
 if __name__ == '__main__':
     print('Starting Python Flask Server for House Price Prediction')
     util.load_saved_artifacts()
-    import os
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
